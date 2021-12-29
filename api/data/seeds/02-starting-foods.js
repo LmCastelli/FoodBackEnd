498ed0cf564af4634430f24fbc7dbcf53941fb15
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-12-29 12:38:08 -0600

    finished model

--- api/data/seeds/02-starting-foods.js
+++ api/data/seeds/02-starting-foods.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('food').del()
+  return knex('food')
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
--- api/router/router-model.js
+++ api/router/router-model.js
@@ -3,3 +3,33 @@
 // create new food
 // edit food by id
 // delete by id
+
+const db = require('../data/db-config')
+
+function getAll() {
+    return db('food')
+}
+async function getById(id) {
+    const rows = await db('food').where('food_id', id).first()
+    return rows
+}
+async function create(food) {
+    const [newFood] = await db('food').insert(food, ['food_id', 'name', 'purpose', 'price', 'delivers', 'unhealthy', 'heavy'])
+    return newFood
+}
+async function editById(id, changes) {
+    const [updated] = await db('food').where('food_id', id).update(changes, ['food_id', 'name', 'purpose', 'price', 'delivers', 'unhealthy', 'heavy'])
+    return updated
+}
+function deleteById(id) {
+    return db('food').where('food_id', id).del()
+}
+
+
+module.exports ={
+    getAll, 
+    getById,
+    create,
+    editById,
+    deleteById,
+}
