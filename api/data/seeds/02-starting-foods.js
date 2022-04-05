b334093e2ee60976d0814c29ef313a7fe5620aa4
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2022-04-05 17:36:09 -0500

    testing

--- api/data/seeds/02-starting-foods.js
+++ api/data/seeds/02-starting-foods.js
@@ -7,7 +7,7 @@ exports.seed = function(knex) {
       return knex('food').insert([
         {name: 'Noodles and Company', purpose: 'meal', price: 2, delivers: 'yes', unhealthy: "false", heavy: "true"},
         {name: 'Crumble', purpose: 'dessert', price: 3, delivers: 'no', unhealthy: "true", heavy: "true"},
-        {name: 'Starbucks', purpose: 'coffee', price: 3, delivers: 'yes', unhealthy: "false", heavy: "false"}
+        {name: 'Starbucks', purpose: 'coffee', price: 2, delivers: 'yes', unhealthy: "false", heavy: "false"}
       ]);
     });
 };
